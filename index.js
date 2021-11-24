c6f368457d6c7ef62a19568fd7b6eb57fd624828
djibadiawara <djiba.diawara97@gmail.com>
2021-11-24 16:53:51 -0500

    commit tree 3dd79dfb54daf10474eb2e667378e1705e382ab3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(drivers, obj) {
+  return drivers.filter ((element ) => {
+    return element.toLowerCase().startsWith(obj.toLowerCase());
+  })
+}
+
+function fuzzyMatch(drivers, obj) {
+  return drivers.filter ((element ) => {
+    return element.toLowerCase().startsWith(obj.toLowerCase());
+  })
+}
+
+function matchName(drivers, hometown) {
+return drivers.filter((element) => element.name === hometown) 
+
+}
+matchName(drivers, hometown);
